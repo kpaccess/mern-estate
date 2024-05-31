be6b29e4029619536d690e1fb7ca2620742217f9
krishna pradhan <kpaccess@gmail.com>
2024-05-31 14:53:02 -0400

    create and add oAuth component

--- api/controllers/auth.controller.ts
+++ api/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import brcryptjs from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model";
 import { errorHandler } from "../utils/error";
@@ -18,8 +18,8 @@ export const signup = async (
   next: NextFunction
 ) => {
   const { username, email, password } = req.body;
-  const salt = await brcryptjs.genSalt(10);
-  const hashedPassword = brcryptjs.hashSync(password, salt);
+  const salt = await bcryptjs.genSalt(10);
+  const hashedPassword = bcryptjs.hashSync(password, salt);
 
   const newUser = new User({ username, email, password: hashedPassword });
 
@@ -42,7 +42,7 @@ export const signin = async (
     const validUser = await User.findOne({ email });
 
     if (!validUser) return next(errorHandler(404, "User not found"));
-    const validPassword = brcryptjs.compareSync(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET || "");
 
@@ -54,6 +54,54 @@ export const signin = async (
       })
       .status(200)
       .json(rest);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const google = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET || "");
+
+      const { password: pass, ...rest }: RestProps = user.toObject();
+
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+
+      const salt = await bcryptjs.genSalt(10);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, salt);
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-4),
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET || "");
+      const { password: pass, ...rest } = newUser.toObject();
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(rest);
+    }
   } catch (err) {
     next(err);
   }
--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -9,6 +9,7 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -79,6 +80,7 @@ export default function SignIn() {
         >
           {loading ? "...loading" : "Sign In"}
         </button>
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Don&apos;t Have an account ?</p>
