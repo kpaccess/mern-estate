9d30d42847aeb2b64d775c430d9f2e67574c91c7
krishna pradhan <kpaccess@gmail.com>
2024-05-31 09:28:49 -0400

    create sign in API route

--- api/controllers/auth.controller.ts
+++ api/controllers/auth.controller.ts
@@ -41,7 +41,7 @@ export const signin = async (
   try {
     const validUser = await User.findOne({ email });
 
-    if (!validUser) return next(errorHandler(500, "User not found"));
+    if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = brcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET || "");
