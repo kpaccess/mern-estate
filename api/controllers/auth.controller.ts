c6200e4e7fbd0eb83df1e0a45e6332c1c01e8849
krishna pradhan <kpaccess@gmail.com>
2024-05-31 09:28:49 -0400

    create sign in API route

--- api/controllers/auth.controller.ts
+++ api/controllers/auth.controller.ts
@@ -1,6 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import brcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model";
+import { errorHandler } from "../utils/error";
+
+type RestProps = {
+  password: string;
+  _doc: {
+    email: string;
+    password: string;
+  };
+};
 
 export const signup = async (
   req: Request,
@@ -17,6 +27,34 @@ export const signup = async (
     await newUser.save();
     res.status(201).json("user created successfully");
   } catch (err: unknown) {
+    next(err);
+  }
+};
+
+export const signin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) return next(errorHandler(500, "User not found"));
+    const validPassword = brcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET || "");
+
+    const { password: pass, ...rest }: RestProps = validUser.toObject();
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
+  } catch (err) {
     next(err);
   }
 };
--- api/utils/error.ts
+++ api/utils/error.ts
@@ -1,5 +1,11 @@
-export const errorHanlder = (message: string) => {
-  const error = new Error();
-  error.message = message;
+export const errorHandler = (statusCode: number, message: string) => {
+  const error = {
+    errorResponse: {
+      message: "",
+      statusCode: 400,
+    },
+  };
+  error.errorResponse.message = message;
+  error.errorResponse.statusCode = statusCode;
   return error;
 };
