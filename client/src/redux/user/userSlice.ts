ed6276c159d0851ee10b862aa2a1a914941455c3
krishna pradhan <kpaccess@gmail.com>
2024-06-04 08:06:47 -0400

    fix ts error for _id

--- client/src/redux/user/userSlice.ts
+++ client/src/redux/user/userSlice.ts
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 export type User = {
+  _id: string;
   id: string;
   name: string;
   avatar: string;
