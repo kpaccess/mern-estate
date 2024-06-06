e8438000bed0689326877f70886c80ebd877beb5
krishna pradhan <kpaccess@gmail.com>
2024-06-06 13:20:03 -0400

    using PUT instead of POST for update

--- api/routes/listing.route.ts
+++ api/routes/listing.route.ts
@@ -10,6 +10,6 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
-router.post("/update/:id", verifyToken, updateListing);
+router.put("/update/:id", verifyToken, updateListing);
 
 export default router;
