839c4665a0d430eadacb5859ba9773b9f2aaebbd
krishna pradhan <kpaccess@gmail.com>
2024-06-06 13:18:15 -0400

    create update listing api route

--- api/controllers/listing.controller.ts
+++ api/controllers/listing.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import Listing from "../models/listing.model";
 import { errorHandler } from "../utils/error";
+import mongoose from "mongoose";
 
 interface AuthenticateRequest extends Request {
   user?: {
@@ -42,6 +43,38 @@ export const deleteListing = async (
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: "Listing has been deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (
+  req: AuthenticateRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  // Validate ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(errorHandler(400, "Invalid listing ID"));
+  }
+
+  const listing = await Listing.findById(id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+
+  if (req?.user?.id !== listing.userRef.toString()) {
+    return next(errorHandler(401, "You can only update your own listings"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
